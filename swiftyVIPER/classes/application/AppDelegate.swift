82313c7b8aca3cd450b03d8b9e4e716d25dc155e
Kohei Tabata <nerd0geek1@gmail.com>
2016-04-03 22:23:28 +0900

    organize project

---
+++ swiftyVIPER/classes/application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  swiftyVIPER
+//
+//  Created by Kohei Tabata on 2016/04/03.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
